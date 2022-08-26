533c071e180d9cdf70fe2f00bf8e92a99e151bd7
liangminhua <ben302010@live.cn>
2022-08-26 10:34:38 +0800

    save

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "ca55caf0.ghcr.io",
+		LeaderElectionID:       "ecaf1259.my.domain",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
